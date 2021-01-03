4bee6606fb385b161d433f5457175822dc3b78f8
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-01-03 16:05:01 +0200

    fixed content

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-function showForm(){
+function showForm() {
     let x = document.getElementById("formShow");
-    x.style.display = "inline-block";
+    if (x.style.display === 'flex'){
+        x.style.display = 'none';
+    }else{
+        x.style.display = "flex";
+    }       
+
+}
+function showRegister() {
+    let y = document.getElementById("registerShowHide");
+    y.style.display = "flex";
+    let parentEl = document.getElementById('main-content-all');
+    parentEl.style.justifyContent = 'flex-end';
+
+}
+
+function checkMatchPw() {
+    let password1 = $("#currPw").val();
+    let confirmPass = $("#confirmPW").val();
+
+    if (password1 !== confirmPass) {
+        $("#divCheckPasswordMatch").hmtl("Passwords don't match!");
+    } else {
+        $("#divCheckPasswordMatch").hmtl("Passwords match");
+    }
 }
